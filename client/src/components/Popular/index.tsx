93d8d96b2ebd1bc0e51468a61a458f8734333db8
rohitman47 <rohitreddy.gangwar@gmail.com>
2022-07-28 16:46:45 +0530

    Add scroll functionality

--- client/src/components/Popular/index.tsx
+++ client/src/components/Popular/index.tsx
@@ -1,7 +1,7 @@
 import Card from "@components/Card";
 import Loader from "@components/Loader";
 import graphqlFetch from "@utils/helpers/graphqlFetch";
-import { memo, useEffect, useState } from "react";
+import { memo, useEffect, useRef, useState } from "react";
 import { useQuery } from "react-query";
 import { Popular } from "../../types/graphql";
 import "./styles.scss";
@@ -33,12 +33,30 @@ function PopularPage() {
   if (isLoading) {
     return <Loader />;
   }
+  const scrollRef = useRef<HTMLDivElement | null>(null);
+  if (scrollRef.current) {
+    scrollRef.current.onwheel = (e) => {
+      scrollRef.current?.scrollTo({ left: e.deltaY * 2 + scrollRef.current.scrollLeft });
+    };
+  }
 
+  const handleLeft = (e: React.MouseEvent<HTMLButtonElement>) => {
+    scrollRef.current?.scrollTo({ left: scrollRef.current.scrollLeft - 48 });
+  };
+  const handleRight = (e: React.MouseEvent<HTMLButtonElement>) => {
+    scrollRef.current?.scrollTo({ left: scrollRef.current.scrollLeft + 48 });
+  };
   return (
-    <div className="popular">
+    <div className="popular" ref={scrollRef}>
+      {/* <button className="scroll-btn left" onClick={handleLeft}>
+        L
+      </button> */}
       {data.popular.map((obj: any, i: any) => (
         <Card data={obj} key={i} />
       ))}
+      {/* <button className="scroll-btn right" onClick={handleRight}>
+        R
+      </button> */}
     </div>
   );
 }
